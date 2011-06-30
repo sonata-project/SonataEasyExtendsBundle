98a7e5b4a473ddd28c8accdcb81a4ef6b023cbd1
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2011-06-30 16:48:03 +0400

    OrmGenerator fix

--- Generator/OrmGenerator.php
+++ Generator/OrmGenerator.php
@@ -136,7 +136,7 @@ class OrmGenerator implements GeneratorInterface
             } else {
                 $output->writeln(sprintf('   + <info>%sRepository</info>', $name));
 
-                $string = Mustache::replace($this->getOrmMetadata()->getEntityRepositoryTemplate(), array(
+                $string = Mustache::replace($this->getEntityTemplate(), array(
                     'extended_namespace'    => $bundleMetadata->getExtendedNamespace(),
                     'name'                  => $name,
                     'namespace'             => $bundleMetadata->getNamespace()
