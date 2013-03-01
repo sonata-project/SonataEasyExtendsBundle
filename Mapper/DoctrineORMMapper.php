5f3e9e61f13fc8174168a12322e7b13d03ff4334
Giulio De Donato <liuggio@gmail.com>
2013-03-01 14:03:15 +0100

    added InheritanceType, Discriminator, and Discriminator Column, and ORMMapper test

--- Mapper/DoctrineORMMapper.php
+++ Mapper/DoctrineORMMapper.php
@@ -17,6 +17,12 @@ class DoctrineORMMapper implements EventSubscriber
 {
     protected $associations;
 
+    protected $discriminators;
+
+    protected $discriminatorColumns;
+
+    protected $inheritanceTypes;
+
     protected $doctrine;
 
     protected $indexes;
@@ -26,11 +32,15 @@ class DoctrineORMMapper implements EventSubscriber
      * @param array                                   $associations
      * @param array                                   $indexes
      */
-    public function __construct($doctrine, $associations = array(), $indexes = array())
+    public function __construct($doctrine, $associations = array(), $indexes = array(), $discriminators = array(), $discriminatorColumns = array(), $inheritanceTypes = array())
     {
         $this->doctrine = $doctrine;
         $this->associations = $associations;
         $this->indexes = $indexes;
+        $this->discriminatorColumns = $discriminatorColumns;
+        $this->discriminators = $discriminators;
+        $this->inheritanceTypes = $inheritanceTypes;
+
     }
 
     /**
@@ -56,6 +66,51 @@ class DoctrineORMMapper implements EventSubscriber
 
         $this->associations[$class][$field] = $options;
     }
+
+    /**
+     * Add a discriminator to a class.
+     *
+     * @param  string  $class               The Class
+     * @param  string  $key                 Key is the database value and values are the classes
+     * @param  string  $discriminatorclass  The mapped class
+     *
+     * @return void
+     */
+    public function addDiscriminator($class, $key, $discriminatorClass)
+    {
+        if (!isset($this->discriminators[$class])) {
+            $this->discriminators[$class] = array();
+        }
+
+        if (!isset($this->discriminators[$class][$key])) {
+            $this->discriminators[$class][$key] = $discriminatorClass;
+        }
+    }
+
+    /**
+     * @param string $class
+     * @param array $columnDef
+     * @return void
+     */
+    public function addDiscriminatorColumn($class, array $columnDef)
+    {
+        if (!isset($this->discriminatorColumns[$class])) {
+            $this->discriminatorColumns[$class] = $columnDef;
+        }
+    }
+    /**
+     * @param string $class
+     * @param string $type
+     *
+     * @return void
+     */
+    public function addInheritanceType($class, $type)
+    {
+        if (!isset($this->inheritanceTypes[$class])) {
+            $this->inheritanceTypes[$class] = $type;
+        }
+    }
+
 
     /**
      * @param string $class
@@ -85,6 +140,10 @@ class DoctrineORMMapper implements EventSubscriber
 
         $this->loadAssociations($metadata);
         $this->loadIndexes($metadata);
+
+        $this->loadDiscriminatorColumns($metadata);
+        $this->loadDiscriminators($metadata);
+        $this->loadInheritanceTypes($metadata);
     }
 
     /**
@@ -109,6 +168,75 @@ class DoctrineORMMapper implements EventSubscriber
 
                     call_user_func(array($metadata, $type), $mapping);
                 }
+            }
+        } catch (\ReflectionException $e) {
+            throw new \RuntimeException(sprintf('Error with class %s : %s', $metadata->name, $e->getMessage()), 404,  $e);
+        }
+    }
+
+    /**
+     * @param ClassMetadataInfo $metadata
+     *
+     * @throws \RuntimeException
+     */
+    private function loadDiscriminatorColumns(ClassMetadataInfo $metadata)
+    {
+
+        if (!array_key_exists($metadata->name, $this->discriminatorColumns)) {
+            return;
+        }
+
+        try {
+            if (isset($this->discriminatorColumns[$metadata->name])) {
+                $arrayDiscriminatorColumns = $this->discriminatorColumns[$metadata->name];
+                if (isset($metadata->discriminatorColumn)) {
+                    $arrayDiscriminatorColumns = array_merge($metadata->discriminatorColumn, $this->discriminatorColumns[$metadata->name]);
+                }
+                $metadata->setDiscriminatorColumn($arrayDiscriminatorColumns);
+            }
+        } catch (\ReflectionException $e) {
+            throw new \RuntimeException(sprintf('Error with class %s : %s', $metadata->name, $e->getMessage()), 404,  $e);
+        }
+    }
+
+    /**
+     * @param ClassMetadataInfo $metadata
+     *
+     * @throws \RuntimeException
+     */
+    private function loadInheritanceTypes(ClassMetadataInfo $metadata)
+    {
+
+        if (!array_key_exists($metadata->name, $this->inheritanceTypes)) {
+            return;
+        }
+        try {
+            if (isset($this->inheritanceTypes[$metadata->name])) {
+
+                $metadata->setInheritanceType($this->inheritanceTypes[$metadata->name]);
+            }
+        } catch (\ReflectionException $e) {
+            throw new \RuntimeException(sprintf('Error with class %s : %s', $metadata->name, $e->getMessage()), 404,  $e);
+        }
+    }
+
+    /**
+     * @param ClassMetadataInfo $metadata
+     *
+     * @throws \RuntimeException
+     */
+    private function loadDiscriminators(ClassMetadataInfo $metadata)
+    {
+         if (!array_key_exists($metadata->name, $this->discriminators)) {
+            return;
+        }
+
+        try {
+            foreach ($this->discriminators[$metadata->name] as $key => $class) {
+                if (in_array($key, $metadata->discriminatorMap)) {
+                    continue;
+                }
+                $metadata->setDiscriminatorMap(array($key=>$class));
             }
         } catch (\ReflectionException $e) {
             throw new \RuntimeException(sprintf('Error with class %s : %s', $metadata->name, $e->getMessage()), 404,  $e);
