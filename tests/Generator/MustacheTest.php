539c1d81880b575e71acb3480cb8be2a1ca4b516
Jordi Sala Morales <jordism91@gmail.com>
2017-12-10 13:29:04 +0100

    Fix cs

--- tests/Generator/MustacheTest.php
+++ tests/Generator/MustacheTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -16,7 +18,7 @@ use Sonata\EasyExtendsBundle\Generator\Mustache;
 
 class MustacheTest extends TestCase
 {
-    public function testMustache()
+    public function testMustache(): void
     {
         $this->assertEquals(Mustache::replace(' Hello {{ world }}', [
           'world' => 'world',
