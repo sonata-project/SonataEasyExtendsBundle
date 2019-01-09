4c605ad6e981a54fca3955cdb26ee3c1bbc94fde
Grégoire Paris <postmaster@greg0ire.fr>
2019-01-09 21:50:44 +0100

    Comply with BC php7 cs rules

--- src/Bundle/PhpcrMetadata.php
+++ src/Bundle/PhpcrMetadata.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -121,7 +123,7 @@ class PhpcrMetadata
             $f->in($this->getMappingDocumentDirectory());
 
             foreach ($f->getIterator() as $file) {
-                $name = explode('.', basename($file));
+                $name = explode('.', basename((string) $file));
                 $names[] = $name[0];
             }
         } catch (\Exception $e) {
