14b99e4c2065e5503cae3ac7b4ade8031f1f7485
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-02-12 03:13:34 +0100

    Applied fixes from FlintCI

--- src/Mapper/DoctrineORMMapper.php
+++ src/Mapper/DoctrineORMMapper.php
@@ -322,7 +322,9 @@ class DoctrineORMMapper implements EventSubscriber
             }
         } catch (\ReflectionException $e) {
             throw new \RuntimeException(
-                sprintf('Error with class %s : %s', $metadata->name, $e->getMessage()), 404, $e
+                sprintf('Error with class %s : %s', $metadata->name, $e->getMessage()),
+                404,
+                $e
             );
         }
     }
